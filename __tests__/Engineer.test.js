33a43d510dc622eae8557dd35d271345960c3fd3
Chip Long <chip_long@yahoo.com>
2021-05-15 13:12:16 -0600

    added Engineer tests - continued fixing errors in other tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,56 @@
+const Employee = require("../lib/Employee");
+const Manager = require("../lib/Engineer");
+
+// valid values
+const name = "John";
+const id = 1234;
+const email = "email@test.com";
+const github = "Chip-L";
+
+describe("Engineer", () => {
+  describe("Initialization", () => {
+    it("should extend the Employee object with an github user name", () => {
+      const obj = new Manager(name, id, email, github);
+
+      expect(obj.name).toEqual(name);
+      expect(obj.id).toEqual(id);
+      expect(obj.email).toEqual(email);
+      expect(obj.github).toEqual(github);
+    });
+
+    it("should throw an error if 'github' is an empty string", () => {
+      const cb = () => new Engineer(name, id, email, "");
+      const err = new Error(
+        "Expected parameter 'github' to be a non-empty string"
+      );
+
+      expect(cb).toThrowError(err);
+    });
+
+    //   it("should throw an error if 'github' is not a string", () => {
+    //     const cb = () => Engineer(name, id, email, 3);
+    //     const err = new Error(
+    //       "Expected parameter 'name' to be a non-empty string"
+    //     );
+    // });
+
+    // describe("getGithub", () => {
+    //   it("should return the object's Github name", () => {
+    //     const obj = new Engineer(name, id, email, github);
+
+    //     const result = obj.getGithub();
+
+    //     expect(result).toEqual(github);
+    //   });
+  });
+
+  describe("getRole", () => {
+    it("should return 'Engineer'", () => {
+      const obj = new Employee(name, id, email, github);
+
+      const result = obj.getRole();
+
+      expect(result).toEqual("Engineer");
+    });
+  });
+});
