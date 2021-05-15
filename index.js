918b5f15f45f32c55f019183ee99fb1844936502
Chip Long <chip_long@yahoo.com>
2021-05-15 16:08:25 -0600

    added menu and engineer questions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require("inquirer");
 const chalk = require("chalk");
 const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 // arrays for questions
 const employeeQuestions = [
@@ -61,7 +63,40 @@ function dispManagerQuestions() {
       console.log("Please try again");
       dispManagerQuestions();
     }
-    // dispMenu();
+    dispMenu();
+  });
+}
+
+function dispEngineerQuestions() {
+  const questions = [...employeeQuestions, ...engineerQuestions];
+
+  inquirer.prompt(questions).then((answers) => {
+    try {
+      team.push(
+        new Engineer(answers.name, answers.id, answers.email, answers.github)
+      );
+    } catch (error) {
+      console.log(chalk.redBright(error));
+      console.log("Please try again");
+      dispEngineerQuestions();
+    }
+    dispMenu();
+  });
+}
+
+function dispMenu() {
+  inquirer.prompt(menuQuestion).then((option) => {
+    console.log(option);
+    switch (option.next) {
+      case "Engineer":
+        dispEngineerQuestions();
+        break;
+      case "Intern":
+        //dispInternQuestions()
+        break;
+      default:
+      //makeHTMLFile()
+    }
   });
 }
 
