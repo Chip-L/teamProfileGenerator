13f7bfa2b0466328b48cf2f9d9e786ed2328dfbe
Chip Long <chip_long@yahoo.com>
2021-05-16 17:09:55 -0600

    copy CSS working

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -2,24 +2,37 @@ const fs = require("fs");
 const path = require("path");
 
 function makeDistFolder() {
-  const distFolder = "./dist"; //path.join(process.cwd(), "dist");
+  const distFolder = path.join(process.cwd(), "dist");
 
+  // console.log(fs.existsSync(distFolder));
   try {
     if (!fs.existsSync(distFolder)) {
-      console.log("mkdir");
+      // console.log("mkdir");
       fs.mkdirSync(distFolder);
     }
   } catch (err) {
     console.error(err);
   }
-  console.log(fs.existsSync(distFolder));
+  // console.log(fs.existsSync(distFolder));
+}
+
+function copyCSS() {
+  const localCSS = path.join(process.cwd(), "src", "bootstrap.css");
+  const newCSS = path.join(process.cwd(), "dist", "bootstrap.css");
+
+  //  By default, destination is overwritten if it already exists.
+  fs.copyFile(localCSS, newCSS, (err) =>
+    err ? console.log("Error Found:", err) : true
+  );
 }
 
 function generateHTML(arrTeam) {
   console.log(arrTeam);
 
   // make dist folder
+  makeDistFolder();
   // copy css to dist folder
+  copyCSS();
   // copy header template to dist folder
   // append manager card html
   // append Engineer and Intern container (if arrTeam.length>0)
@@ -33,3 +46,4 @@ function generateHTML(arrTeam) {
 module.exports = generateHTML;
 
 makeDistFolder();
+copyCSS();
