c7e4f8a85b0055f71c62791d870ddf495c7b8663
Chip Long <chip_long@yahoo.com>
2021-05-16 14:35:29 -0600

    starting on generate html. have footer template and check on dist directory creation

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,13 +1,35 @@
 const fs = require("fs");
+const path = require("path");
+
+function makeDistFolder() {
+  const distFolder = "./dist"; //path.join(process.cwd(), "dist");
+
+  try {
+    if (!fs.existsSync(distFolder)) {
+      console.log("mkdir");
+      fs.mkdirSync(distFolder);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+  console.log(fs.existsSync(distFolder));
+}
 
 function generateHTML(arrTeam) {
-  let doc = "";
+  console.log(arrTeam);
 
-  // read header
+  // make dist folder
+  // copy css to dist folder
+  // copy header template to dist folder
+  // append manager card html
+  // append Engineer and Intern container (if arrTeam.length>0)
   // iterate through arrTeam and populate appropriate divs
-  // read footer
-
-  return doc;
+  //  append engineer cards
+  //  append intern cards
+  // append close Engineer and Intern container
+  // append footer template
 }
 
 module.exports = generateHTML;
+
+makeDistFolder();
