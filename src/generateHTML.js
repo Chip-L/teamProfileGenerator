c3bb6f99964f50a5acfd5f05ab5e2af4789549fe
Chip Long <chip_long@yahoo.com>
2021-05-16 11:37:49 -0600

    base html setup

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,3 +1,13 @@
-function generateHTML(arrTeam) {}
+const fs = require("fs");
+
+function generateHTML(arrTeam) {
+  let doc = "";
+
+  // read header
+  // iterate through arrTeam and populate appropriate divs
+  // read footer
+
+  return doc;
+}
 
 module.exports = generateHTML;
